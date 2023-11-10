3ffac64617c5ef891aff37f7e94fc324d7482cc7
SpreadFreeTCC <enzzovasconcelos44@gmail.com>
2023-11-10 00:59:07 -0300

    voltei

--- config/bdd.cjs
+++ config/bdd.cjs
@@ -2,17 +2,11 @@ var mysql = require("mysql");
 
 
 module.exports = function(){
-  var connection = mysql.createConnection({
-  //  return mysql.createConnection({
+ return mysql.createConnection({
     host: "viaduct.proxy.rlwy.net",
     user: "root",
     password: "e-gAeCGc123C5hhbbH34A4H5hhAG4a-F",
-    database: "Tables",
+    database: "railway",
     port: 57514
   });
-
-  connection.connect();
-  return connection
-
-  }
-  
+}
