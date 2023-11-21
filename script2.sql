fd1b41a8d68794c1f875bf19f0b5ff122a6ef7bb
eaemorgado <eaemorgado@gmail.com>
2023-11-21 15:02:51 -0300

    at

--- script2.sql
+++ script2.sql
@@ -1,45 +1,66 @@
-CREATE DATABASE `ziuu`;
-USE `ziuu`;
 
-CREATE TABLE usuarios (
-  id int not null auto_increment primary key,
-  nome varchar(255) NOT NULL,
-  email varchar(60) NOT NULL,
-  senha longtext NOT NULL,
-  usuario varchar(55),
-  img_usuario varchar(255) default '/img/fotodeperfil3.png',
-  id_tipo_usuario int not null default '1'
+
+create database IF NOT EXISTS NixWear;
+use NixWear;
+
+DROP TABLE IF EXISTS usuario;
+create table usuario(
+id_usuario int primary key auto_increment not null,
+user_name varchar(30) not null,
+email varchar(100) not null,
+senha varchar(60) not null,
+telefone varchar(14),
+foto varchar(150) default 'imagens/Group 9 (1).png',
+tipo_usuario int default '1'
+);
+
+DROP TABLE IF EXISTS vender_nix;
+create table vender_nix(
+id_vender_nix int primary key auto_increment not null,
+usuario_id_usuario int,
+nome_loja varchar(30) not null,
+cpf varchar(11) not null,
+nome_dono varchar(30) not null
+);
+
+DROP TABLE IF EXISTS cartao;
+create table cartao(
+id_cartao int primary key auto_increment not null,
+usuario_id_usuario int,
+card_number varchar(16) not null,
+card_name varchar(30) not null,
+expiry_date date not null,
+cvv varchar(3) not null
+);
+
+DROP TABLE IF EXISTS endereco;
+create table endereco(
+id_endereco int primary key auto_increment not null,
+usuario_id_usuario int,
+cep varchar(9) not null,
+estado varchar(45) not null,
+cidade varchar(45) not null,
+bairro varchar(45) not null,	
+rua_numero varchar(45) not null,
+complemento varchar(45) not null
+);
+
+DROP TABLE IF EXISTS produto_nix;
+create table produto_nix(
+id_produto_nix int primary key auto_increment not null,
+img_produto varchar(255),
+nome_produto varchar(100),
+descricao_produto varchar (255),
+preco int,
+loja varchar(30),
+id_vendedor int
 );
 
 
-CREATE TABLE divulgacao (
-  id_divulgacao int not null auto_increment primary key, 
-  img_divulgacao varchar(255),
-  usuario_divulgacao varchar(60) NOT NULL,
-  titulo_divulgacao varchar(255) NOT NULL,
-  id_usuario int,
-  foreign key (id_usuario) references usuarios(id)
+create table carrinho (
+	id_carrinho int auto_increment primary key,
+	id_usuario int,
+	id_produto_nix int,
+    foreign key (id_produto_nix) references produto_nix(id_produto_nix),
+    foreign key (id_usuario) references usuario(id_usuario) 
 );
-
-
-
-CREATE TABLE comunidadeartes (
-  id_comunidade int not null auto_increment primary key,
-  id_usuario int,
-  usuario varchar(55),
-  mensagem varchar(1000),
-  img_divulgacao varchar (255)
-);
-
-
-create table `tipo_usuario` (
-	id_tipo_usuario int not null auto_increment,
-	tipo_usuario varchar (25) default null,
-    inscricao_usuario varchar (155) default null,
-    status_tipo_usuario int default '1',
-    primary key (`id_tipo_usuario`)
-    
-);	
-
-
-
